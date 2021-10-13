250f01b7f129c1efd8c622f73acb4b363e6b2ee8
Adrian-Sixense <adrianrebisz@gmail.com>
2021-10-13 15:15:16 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-#include <Windows.h>
 
 using namespace std;
 
-int sekundy;
 
 int main()
 {
-    cout << "Czy aby na pewno chcesz zniszczyc swoj komputer? " << endl << endl;
-    cout << "Podaj liczbe sekund do spalenia jednostki: " << endl;
-    cin >> sekundy;
-    for (int i=sekundy; i>=0; i--)
-    {
-        Sleep(1000);
-        cout << "Pozostalo " << i << " sekund twojemu komputerowi." << endl;
+    
 
-    }
 
     return 0;
 }
